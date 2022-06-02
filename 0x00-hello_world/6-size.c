0d733b10d39c222171da46e24ae53089614f9205
Shayo090 <abdulsayoboy@gmail.com>
2022-06-02 15:10:47 -0700

    final main task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	double d;
+	char c;
+	float f;
+
+	printf("The size of an int, %ln.\n", (unsigned long)sizeof(i));
+	printf("The size of a double, %ln.\n", (unsigned long)sizeof(d));
+	printf("The size of a char, %ln.\n", (unsigned long))sizeof(c));
+	printf("The size of a float, %ln.\n", (unsignedd long)sizeof(f));
+
+		return (0);
+}
