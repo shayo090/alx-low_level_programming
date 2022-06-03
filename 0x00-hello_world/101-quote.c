6d926af7fbdea8afb8e45dfbabb1967a6348a72c
Shayo090 <abdulsayoboy@gmail.com>
2022-06-02 17:19:21 -0700

    finale

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,9 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
-{
+int main(void) 
+{ 
 	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
-		return (1);
+
+return (1);
 }
