ff3355a566f97063c22fec155724ba8a7b1e8375
Shayo090 <abdulsayoboy@gmail.com>
2022-06-03 19:15:57 -0700

    11 again

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,10 +16,8 @@ int main(void)
 	{
 		for (b = a + 1; b <= 99; b++)
 		{
-			putchar((a / 10) + '0');
 			putchar((a % 10) + '0');
 			putchar(' ');
-			putchar((b / 10) + '0');
 			putchar((b % 10) + '0');
 
 			if (a == 98 && b == 99)
