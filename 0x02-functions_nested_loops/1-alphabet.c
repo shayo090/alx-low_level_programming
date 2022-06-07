a4f9880349be4c34876056505aec40a17e939a81
Shayo090 <abdulsayoboy@gmail.com>
2022-06-07 14:42:19 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char i;
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar('i');
+	_putchar('\n');
+
+	return (0);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-read me
+0x02. C - Functions, nested loops
