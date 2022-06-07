a39995ce864114599936b1a95e56048a8bbad12c
Shayo090 <abdulsayoboy@gmail.com>
2022-06-07 15:56:25 -0700

    222

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10: prints alphabet ten times
+ * print_alphabet_x10 - displays alphabet 10 times.
  *
  * Return: Always 0.
  */
@@ -11,6 +11,7 @@ void print_alphabet_x10(void)
 	int i;
 	int no_lines;
 
+	no_lines = '0';
 	while (no_lines <= '9')
 	{
 		for (i = 'a'; i <= 'z'; i++)
