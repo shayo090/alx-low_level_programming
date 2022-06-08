59b2f10307ee40e910bbd4f211571669d201d500
Shayo090 <abdulsayoboy@gmail.com>
2022-06-07 17:54:48 -0700

    6

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+int print_sign(int n)
+{
+	int i;
+	
+	if (i > '1')
+		return (1);
+		_putchar('+');
+	else if (i = '0')
+		return (0);
+		_putchar('0');
+	else
+		return (-1);
+		_putchar('-');
+}
