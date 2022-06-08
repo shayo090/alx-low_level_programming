adfee189526248311ce9475cb4601abb598e9a7e
Shayo090 <abdulsayoboy@gmail.com>
2022-06-07 18:13:23 -0700

    555555

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,20 +8,19 @@
 
 int print_sign(int n)
 {
-	
 	if (n > 0)
 	{
 		return (1);
-		_putchar('+');
+		_putchar('+1');
 	}
 	else if (n == 0)
 	{
 		return (0);
-		_putchar('0');
+		_putchar('00');
 	}
 	else
 	{
 		return (-1);
-		_putchar('-');
+		_putchar('-1');
 	}
 }
