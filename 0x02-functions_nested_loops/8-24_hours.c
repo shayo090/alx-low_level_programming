964f8bb0a908505e2bbbacc1bf364c0f75bf44e5
Shayo090 <abdulsayoboy@gmail.com>
2022-06-09 00:44:08 -0700

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,1 +1,26 @@
+#include "main.h"
 
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: Always 0 Success
+ *
+ */
+
+void jack_bauer(void)
+{
+	int H, M;
+
+	for (H = 0; H < 24; H++)
+	{
+		for (M = 0; M < 60; M++)
+		{
+			_putchar((H / 10) + '0');
+			_putchar((H % 10) + '0');
+			_putchar(':');
+			_putchar((M / 10) + '0');
+			_putchar((M % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
