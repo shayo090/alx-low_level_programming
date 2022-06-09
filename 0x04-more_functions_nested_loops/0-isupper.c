c4692c61b4ce911524e32eec8e4e313d6151c445
Shayo090 <abdulsayoboy@gmail.com>
2022-06-09 04:59:31 -0700

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * 0-isupper - checks for upper case characters
+ * @c: upper case value 
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+	int i;
+
+	for(i = 'A'; i <= 'Z'; i++)
+		if (c = i)
+		{
+			return (1);
+		}
+	return (0);
+}
