39446507f909b280cb05a6b666261e6a217b9815
Shayo090 <abdulsayoboy@gmail.com>
2022-06-13 13:19:51 -0700

    six

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,11 +11,11 @@
 void puts2(char *str)
 {
 	int i;
-	for (i = 0; str[i] != 0, i++)
+	for (i = 0; str[i] != 0; i++;)
 	{
 		if (i % 2 == 0)
 		{
-			_puthcar(str[i]);
+			_putchar(str[i]);
 		}
 	}
 }
