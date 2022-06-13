9258dbea5e03d01c580f0810fe07b2050bd9d46c
Shayo090 <abdulsayoboy@gmail.com>
2022-06-13 16:46:50 -0700

    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,29 @@
+julien@ubuntu:~/0x05$ cat 9-main.c
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - function that copies strings
+ *
+ * @src: source
+ *
+ * @dest: destination
+ *
+ * Return: Always 0.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int len;
+	int i;
+
+	len = 0;
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len + 1; i++)
+		*dest[i] == *src[i];
+	return (dest);
+}
