71e9bbb1575573519a49f203c53dcbe0fba80709
Shayo090 <abdulsayoboy@gmail.com>
2022-06-13 08:51:54 -0700

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - function to print string
+ *
+ * @str : string to be printed
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchcar('\n');
+}
