5e969881090886cf05ac3672f2202982d569517b
Shayo090 <abdulsayoboy@gmail.com>
2022-06-13 13:04:31 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * put2 - a function that prints every other character of a string
+ *
+ * @str: string to print
+ *
+ * Return : void
+ */
+
+void puts2(char *str)
+{
+	int i;
+	for (i = 0; str[i] != 0, i++)
+	{
+		if (i % 2 == 0)
+		{
+			_puthcar(str[i]);
+		}
+	}
+}
