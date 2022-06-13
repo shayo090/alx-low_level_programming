1e773051649dbbf6061a859f104b9e76e406210b
Shayo090 <abdulsayoboy@gmail.com>
2022-06-13 16:14:18 -0700

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -18,8 +18,8 @@ void print_array(int *a, int n)
 	for (; i < n; i++)
 	{
 		printf("%d", a[i]);
-		if (i != n - 1 || i < n - 1)
+		if (i != n - 1)
 			printf(", ");
 	}
-	_putchar('\n');
+	printf("\n");
 }
