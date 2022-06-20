78ec474ca015ff6766b71c165d7256fdadb861a4
Shayo090 <abdulsayoboy@gmail.com>
2022-06-20 12:14:04 -0700

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -19,10 +19,14 @@ char *_strstr(char *haystack, char *needle)
 	{
 		for (j = 0; needle[j]; j++)
 		{
-			if (haystack[i + j] == needle[j])
+			if (haystack[i + j] != needle[j])
 			{
-				return (haystack + i);
+				break;
 			}
+		}
+		if (needle[j] == '\0')
+		{
+			return (haystack + i);
 		}
 	}
 	return (0);
