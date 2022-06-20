cd4bbaa5fa7b527f649fb1a716b8f914c5f76a2b
Shayo090 <abdulsayoboy@gmail.com>
2022-06-20 16:29:05 -0700

    p2p

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * set_string - function to set string
+ *
+ * @s : pointer to a pointer
+ *
+ * @to : pointer to a string
+ *
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
