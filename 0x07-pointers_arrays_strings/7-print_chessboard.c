637bfedbdbd4d7070a26eab708e1cd8e2d9ba661
Shayo090 <abdulsayoboy@gmail.com>
2022-06-20 12:20:25 -0700

    still working on it

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_chessboard - function that prints chessboard
+ *
+ * @a : rows of the chess board
+ *
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
