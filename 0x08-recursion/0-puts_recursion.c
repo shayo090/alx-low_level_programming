ee038c5a30b1a5c322c7920eef9925456da45b82
Shayo090 <abdulsayoboy@gmail.com>
2022-06-22 10:53:33 -0700

    string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,10 +10,12 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	int len;
+
+	if (s[len] == '\0')
 	{
 		_putchar('\n');
 	}
-	_putchar(*s);
+	_putchar(s[len]);
 	_puts_recursion(s + 1);
 }
