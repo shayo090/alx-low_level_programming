51faf1fcaa5188a2096c1da857acbd28e83235e3
Shayo090 <abdulsayoboy@gmail.com>
2022-06-24 18:22:29 -0700

    library

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - function that copies strings
+ *
+ * @src: source
+ *
+ * @dest: destination
+ *
+ * Return: Always 0.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int len;
+	int i;
+
+	len = 0;
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len + 1; i++)
+		dest[i] = src[i];
+	return (dest);
+}
