6f041cddbfc9674871ab0509565d85e80ea214fc
Shayo090 <abdulsayoboy@gmail.com>
2022-06-28 00:46:42 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,11 +20,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == 0)
 	{
-		s1 = '\0';
+		len1 = 0;
 	}
 	if (s2 == 0)
 	{
-		s2 = '\0';
+		len2 = 0;
 	}
 	for (len1 = 0; s1[len1] != '\0'; len1++)
 
