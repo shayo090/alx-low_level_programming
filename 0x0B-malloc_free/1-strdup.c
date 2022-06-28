5bef17196cff55dbe4f75f9477edd126399a4466
Shayo090 <abdulsayoboy@gmail.com>
2022-06-27 23:49:11 -0700

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,27 +11,22 @@
 
 char *_strdup(char *str)
 {
+	char *s;
 	int len;
 	int i;
-	char *s;
 
+	len = 0;
 	if (str == NULL)
-	{
 		return (NULL);
-	}
 
-	for (len = 0; str[len] != '\0'; len++)
+	while (str[len] != 0)
+		len++;
+	s = malloc((len + 1) * sizeof(char));
 
-	s = (char*)malloc(sizeof(char) * (len + 1));
-
-	if (s == NULL)
-	{
+	if (A == NULL)
 		return (NULL);
-	}
 
-	for (i = 0; i < len; i++)
-	{
+	for (i = 0; i <= len; i++)
 		s[i] = str[i];
-	}
 	return (s);
 }
