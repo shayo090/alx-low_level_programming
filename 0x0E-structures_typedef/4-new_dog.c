2ea736354435503d8fa00df5013de664403b6ec0
Shayo090 <abdulsayoboy@gmail.com>
2022-07-04 14:08:26 -0700

    structures and typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,80 +2,50 @@
 #include "dog.h"
 
 /**
- * new_dog - a function that creates a new dog
- *
- * @name : name og dog
- *
- * @age : dog's age
- *
- * @owner : nmae of owner
- *
- * Return :pointer of newly created dog
+ * new_dog - creates a new dog structure
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ * Return: returns a pointer to newly created dog structure
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	int len_name;
-	int len_owner;
-	char *new_name;
-	char *new_owner;
-	int i;
+	int len_name = 0, len_owner = 0, i;
+	char *new_name, *new_owner;
 
 	if (name == NULL || owner == NULL)
 	{
 		return (NULL);
 	}
 
-	len_name = 0;
 	while (name[len_name])
-	{
 		len_name++;
-	}
 
-	len_owner = 0;
 	while (owner[len_owner])
-	{
 		len_owner++;
-	}
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
-	{
 		return (NULL);
-	}
 
 	new_name = malloc(len_name + 1);
 	if (new_name == NULL)
-	{
 		return (NULL);
-	}
-	else
-	{
-		for (i = 0; name[i] != '\0'; i++)
-		{
-			new_name[i] = name[i];
-		}
-	}
+	for (i = 0; name[i] != '\0'; i++)
+		new_name[i] = name[i];
 	new_name[i] = '\0';
 
 	new_owner = malloc(len_owner + 1);
 	if (new_owner == NULL)
-	{
 		return (NULL);
-	}
-	else
-	{
-		for (i = 0; owner[i] != '\0'; i++)
-		{
-			new_owner[i] = owner[i];
-		}
-	}
+	for (i = 0; owner[i] != '\0'; i++)
+		new_owner[i] = owner[i];
 	new_owner[i] = '\0';
 
 	new_dog->name = new_name;
 	new_dog->age = age;
 	new_dog->owner = owner;
-
 	return (new_dog);
 }
