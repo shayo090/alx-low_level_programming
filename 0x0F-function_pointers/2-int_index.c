afe1d4599c969d20d819ff2f0d1ea585483e2077
Shayo090 <abdulsayoboy@gmail.com>
2022-07-06 09:49:44 -0700

    updated

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,8 +1,8 @@
-#include "function_pointers.h"
 #include <stdio.h>
+#include "function_pointers.h"
 
 /**
- * int_index - function that searches for an integer
+ * int_index - a function that searches for an integer
  *
  * @array : the array
  *
