adfc313438673b6fbeb8ca3610779f6def4b03ae
Shayo090 <abdulsayoboy@gmail.com>
2022-07-14 09:00:47 -0700

    repair

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,9 +3,9 @@
 /**
 * print_list - Prints all elements of a list
 *
-* @h: A linked list
+* @h : A linked list
 *
-* Return: The number of nodes
+* Return : The number of nodes
 */
 
 size_t print_list(const list_t *h)
@@ -20,7 +20,7 @@ size_t print_list(const list_t *h)
 		}
 		else
 		{
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%d] %s\n",h->len, h->str);
 		}
 		h = h->next;
 		count++;
