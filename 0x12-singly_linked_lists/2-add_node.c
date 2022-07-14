3f60f2b1283c3e5be909157532a2aafed638991c
Shayo090 <abdulsayoboy@gmail.com>
2022-07-14 07:36:00 -0700

    linked lists

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+* add_node - function that adds a node to the top of a list
+*
+* @head : the pointer to a pointer to a list
+*
+* @str : the string the new node contains
+*
+* Return: pointer to the new node
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *temp;
+
+	if (!head)
+		return (0);
+	temp = malloc(sizeof(list_t));
+	if (!temp)
+	{
+		return (0);
+	}
+	
+	temp->str = strdup(str);
+	temp->len = strlen(str);
+	temp->next = *head;
+	*head = temp;
+	return (temp);
+}
