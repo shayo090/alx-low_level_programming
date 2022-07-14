590507179ce44668effb260a880c6a6e9007060f
Shayo090 <abdulsayoboy@gmail.com>
2022-07-14 14:41:59 -0700

    again

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,6 +6,7 @@
   *
   * Return: The number of nodes
   */
+
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
@@ -13,12 +14,15 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
+		{
 			printf("[0] (nil)\n");
+		}
 		else
+		{
 			printf("[%d] %s\n", h->len, h->str);
+		}
 		count++;
 		h = h->next;
 	}
-
 	return (count);
 }
