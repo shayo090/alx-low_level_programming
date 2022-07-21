6de757a62274d947ebddfb5a33eada93a84774c3
Shayo090 <abdulsayoboy@gmail.com>
2022-07-21 13:41:34 -0700

    bit manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,26 +9,34 @@
  *
  * Return: Nothing
  */
+
 void print_binary(unsigned long int n)
 {
-	unsigned int flag = 0, max = 32768;
-
 	if (n == 0)
 	{
 		_putchar('0');
 		return;
 	}
-	while (max)
-	{
-		if (flag == 1 && (n & max) == 0)
-		{
-			_putchar('0');
-		}
-		else if ((n & max) != 0)
-		{
-			_putchar('1');
-			flag = 1;
-		}
-		max >>= 1;
-	}
+
+	_divide(n);
+}
+
+/**
+  * _divide - division
+  *
+  * @n: number
+  *
+  * Return: nothing
+  */
+void _divide(unsigned long int n)
+{
+	if (n < 1)
+		return;
+
+	_divide(n >> 1);
+
+	if (n & 1)
+		_putchar('1');
+	else
+		_putchar('0');
 }
