17c1c2c22da96a9aa1a8aaf6e45ae08e2c0cc907
Shayo090 <abdulsayoboy@gmail.com>
2022-07-21 13:45:33 -0700

    bit manipulation

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,15 +11,11 @@
  *
  * Return: 1 if it worked, or -1 on error
  */
+
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int max = 0x01;
-
-	max <<= index;
-	if (max == 0)
-	{
+	if (index > sizeof(unsigned long int) * 8)
 		return (-1);
-	}
-	*n |= max;
-	return (1);
+
+	return ((*n |= 1 << index) ? 1 : -1);
 }
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -14,14 +14,11 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int max = 0x01;
-
-	max = ~(max << index);
-	if (max == 0x00)
+	if (index < sizeof(unsigned long int) * 8)
 	{
-		return (-1);
+		*n &= (~(1 << index));
+		return (1);
 	}
 
-	*n &= max;
-	return (1);
+	return (-1);
 }
